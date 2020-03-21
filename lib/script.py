78f9dc22c7672e7fdd1155b9ffa00477a30ee897
DRHood <darryl.r.hood@gmail.com>
2020-03-21 18:57:44 -0400

    1st draft for testing beginninng functionality

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,17 @@
 from capitals import states
 
-print(states)
+print("How well do you know your state capitals?")
+print("Let's find out.")
+
+def begin():
+    input ("Would you like to play a game? (y/n)")
+    if begin == "y":
+        return play_game()
+    else:
+        print("Maybe next time.")
+        return
+
+def play_game():
+    print("This is where the game goes!")
+
+begin()
