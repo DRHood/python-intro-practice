d004d6ae59be2d4e794b5771cd7b02429a57e82c
DRHood <darryl.r.hood@gmail.com>
2020-03-21 21:00:23 -0400

    corrected scoring issue

--- lib/script.py
+++ lib/script.py
@@ -3,10 +3,6 @@ from capitals import states
 print("How well do you know your state capitals?")
 print("Let's find out.")
 
-for state in states:
-    state["correct"] = 0
-    state["incorrect"] = 0
-
 def begin():
     begin = input("Would you like to play a game? (y/n)")
     if begin == "y":
@@ -16,16 +12,20 @@ def begin():
         return
 
 def play_game():
+    correct_count = 0
+    incorrect_count = 0
+
     for state in states:
-        guess_capital = input("What is yhe capital of " + state["name"] + "?")
+        guess_capital = input("What is the capital of " + state["name"] + "?")
 
         if guess_capital == state["capital"]:
             print("Look at you, all smart and stuff!")
-            state["correct"] += 1
+            correct_count += 1
         else:
             print("Yeah...Not quite right.")
-            state["incorrect"] -= 1
-        print("Correct: {}".format(state["correct"]))
-        print("Incorrect: {}".format(state["correct"]))
+            incorrect_count += 1
+
+        print("Correct: {}".format(correct_count))
+        print("Incorrect: {}".format(incorrect_count))
 
 begin()
